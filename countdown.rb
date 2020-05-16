e8d4384ddde54158da76714419d84c7290b0c4f2
isabella g henriques <isabellagh88@gmail.com>
2020-05-16 14:34:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,13 +9,7 @@ def countdown_with_sleep(seconds)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end
+
 
 
 
